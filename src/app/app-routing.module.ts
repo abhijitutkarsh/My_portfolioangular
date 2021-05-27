dc7a96e1bcf0e5e56d0ec38295903765cf7e9340
Abhijit Utkarsh <abhijitu18@gmail.com>
2021-05-28 01:57:03 +0530

    mobileinitials

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ExperienceComponent } from './experience/experience.component';
 import { AboutComponent } from './about/about.component';
 import { EducationComponent } from './education/education.component';
 import { ContactComponent } from './contact/contact.component';
-
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
@@ -14,6 +14,7 @@ const routes: Routes = [
   { path: 'education', component: EducationComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'header', component: HeaderComponent },
 ];
 
 @NgModule({

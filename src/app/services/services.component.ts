84151b97ad85e52162de18a51baabea433c4352b
Abhijit Utkarsh <abhijitu18@gmail.com>
2021-06-27 03:50:44 +0530

    latest

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+
 
 @Component({
   selector: 'app-services',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
-  ngOnInit(): void {
-  }
+  cardLayout = this.breakpointObserver.observe([Breakpoints.Handset, Breakpoints.Tablet]).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return {
+          columns: 1,
 
-}
+          chart: { cols: 1, rows: 1 },
+          table: { cols: 3, rows: 3 },
+          final: { cols: 2, rows: 3 },
+         aj: { cols: 1, rows: 2},
+        };
+      }
+
+      return {
+        columns: 4,
+
+        chart: { cols: 1, rows: 1 },
+        table: { cols: 1, rows: 3 },
+        final: { cols: 1, rows: 3 },
+        aj: { cols: 1, rows: 2 }
+
+      };
+    })
+  );
+
+      // tslint:disable-next-line:member-ordering
+      defaultElevation = 2;
+      // tslint:disable-next-line:member-ordering
+      raisedElevation = 8;
+
+      titles = ['title1', 'title2', 'title3', 'title4', 'title5'];
+      titleSelected: any;
+
+      name = 'Angular';
+
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+
+      }
+
+      // tslint:disable-next-line:typedef
+      select(title: any) {
+        this.titleSelected = title;
+      }
+    }
